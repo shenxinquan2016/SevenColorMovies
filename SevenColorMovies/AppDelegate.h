5300cb1693079a6b995263660618dec1e2133337
董国庆 <yesdgq@yahoo.com>
2016-07-15 14:26:41 +0800

    Initial Commit

---
+++ SevenColorMovies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SevenColorMovies
+//
+//  Created by yesdgq on 16/7/15.
+//  Copyright © 2016年 yesdgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
