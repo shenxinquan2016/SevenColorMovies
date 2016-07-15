ef730e7970c4f1703137b7fc5b4b2f3855edd660
董国庆 <yesdgq@yahoo.com>
2016-07-15 17:54:47 +0800

    Add  初始化配置

---
+++ SevenColorMovies/Configured.h
@@ -0,0 +1,15 @@
+//
+//  Configured.h
+//  SevenColorMovies
+//
+//  Created by yesdgq on 16/7/15.
+//  Copyright © 2016年 yesdgq. All rights reserved.
+//
+
+#ifndef Configured_h
+#define Configured_h
+
+#import <Masonry.h>
+
+
+#endif /* Configured_h */
