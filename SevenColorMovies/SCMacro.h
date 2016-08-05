f30bf0157f332536aee674813daa1f32e8b9be31
董国庆 <yesdgq@yahoo.com>
2016-08-05 09:46:16 +0800

    text

---
+++ SevenColorMovies/SCMacro.h
@@ -0,0 +1,14 @@
+//
+//  SCMacro.h
+//  SevenColorMovies
+//
+//  Created by yesdgq on 16/8/5.
+//  Copyright © 2016年 yesdgq. All rights reserved.
+//
+
+#ifndef SCMacro_h
+#define SCMacro_h
+
+#import "ThirdKeys.h"
+
+#endif /* SCMacro_h */
---
+++ SevenColorMovies/ThirdKeys.h
@@ -0,0 +1,29 @@
+//
+//  ThirdKeys.h
+//  SevenColorMovies
+//
+//  Created by yesdgq on 16/8/5.
+//  Copyright © 2016年 yesdgq. All rights reserved.
+//
+
+#ifndef ThirdKeys_h
+#define ThirdKeys_h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* ThirdKeys_h */
