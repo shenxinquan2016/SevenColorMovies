b5931407f6c4714547ae9aedc37cba3d09ac3773
董国庆 <yesdgq@yahoo.com>
2016-12-05 15:01:44 +0800

    save

--- SevenColorMovies/DONG_BrightnessView.h
+++ SevenColorMovies/DONG_BrightnessView.h
@@ -10,11 +10,6 @@
 
 @interface DONG_BrightnessView : UIView
 
-/** 调用单例记录播放状态是否锁定屏幕方向*/
-@property (nonatomic, assign) BOOL     isLockScreen;
-/** 是否允许横屏,来控制只有竖屏的状态*/
-@property (nonatomic, assign) BOOL     isAllowLandscape;
-
 + (instancetype)sharedBrightnessView;
 
 @end
