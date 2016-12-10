30104288a475b5e9dbd8323b1ff7ec8e1b384c9f
董国庆 <yesdgq@yahoo.com>
2016-12-10 23:19:04 +0800

    save

---
+++ SevenColorMovies/SCDeviceCell.h
@@ -0,0 +1,13 @@
+//
+//  SCDeviceCell.h
+//  SevenColorMovies
+//
+//  Created by yesdgq on 16/12/10.
+//  Copyright © 2016年 yesdgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCDeviceCell : UITableViewCell
+
+@end
---
+++ SevenColorMovies/SCDeviceCell.m
@@ -0,0 +1,24 @@
+//
+//  SCDeviceCell.m
+//  SevenColorMovies
+//
+//  Created by yesdgq on 16/12/10.
+//  Copyright © 2016年 yesdgq. All rights reserved.
+//
+
+#import "SCDeviceCell.h"
+
+@implementation SCDeviceCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
