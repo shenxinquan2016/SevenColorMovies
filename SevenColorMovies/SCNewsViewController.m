caca9db11e7c6e0c415b6ecfcd69b3da79b42e3d
董国庆 <yesdgq@yahoo.com>
2016-12-26 12:00:52 +0800

    save

--- SevenColorMovies/SCNewsViewController.m
+++ SevenColorMovies/SCNewsViewController.m
@@ -28,16 +28,6 @@
 }
 
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 // 禁止旋转屏幕
 - (BOOL)shouldAutorotate{
     return NO;
