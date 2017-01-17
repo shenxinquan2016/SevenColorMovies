228e624a516e29539a954ad68eb2f499ac2296dd
董国庆 <yesdgq@yahoo.com>
2017-01-17 18:34:44 +0800

    save

--- SevenColorMovies/SCLiveViewController.m
+++ SevenColorMovies/SCLiveViewController.m
@@ -248,7 +248,7 @@ static const CGFloat LabelWidth = 95.f;
     [CommonFunc showLoadingWithTips:@""];
     [requestDataManager requestDataWithUrl:LivePageUrl parameters:nil success:^(id  _Nullable responseObject) {
         
-//        DONG_Log(@"==========dic:::%@========",responseObject);
+        DONG_Log(@"==========dic:::%@========",responseObject);
         if (responseObject) {
             NSArray *array = responseObject[@"LiveTvSort"];
             
