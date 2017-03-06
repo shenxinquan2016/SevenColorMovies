8543fbbe40dc44762930f4e166d67fb85a587f30
董国庆 <yesdgq@yahoo.com>
2017-03-06 16:47:30 +0800

    save

--- SevenColorMovies/SCHomePageViewController.m
+++ SevenColorMovies/SCHomePageViewController.m
@@ -676,16 +676,4 @@ static NSString *const footerId = @"footerId";
     return NO;
 }
 
-// 禁止旋转屏幕
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations // 支持哪些方向
-{
-    return UIInterfaceOrientationMaskPortrait;
-}
-
-// 默认显示的方向
-- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
-{
-    return UIInterfaceOrientationPortrait;
-}
-
 @end
