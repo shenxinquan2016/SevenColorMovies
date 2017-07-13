3751f4b8bc075d5990134e4c6e8d34a9efb8c8c9
董国庆 <yesdgq@yahoo.com>
2017-07-13 11:14:20 +0800

    主持人大赛注册

--- SevenColorMovies/SCLovelyBabyRegisterVC.m
+++ SevenColorMovies/SCLovelyBabyRegisterVC.m
@@ -82,7 +82,7 @@
     [_pullSupportTimeLabel setFont :[UIFont fontWithName :@"Helvetica-Bold" size :13.f]];
     [_releaseTimeLabel setFont :[UIFont fontWithName :@"Helvetica-Bold" size :13.f]];
     
-    
+    // 单选框
     [_maleBtn setImage:[UIImage imageNamed:@"Selected"] forState:UIControlStateSelected];
     [_maleBtn setImage:[UIImage imageNamed:@"Unselect"] forState:UIControlStateNormal];
     [_femaleBtn setImage:[UIImage imageNamed:@"Selected"] forState:UIControlStateSelected];
@@ -92,24 +92,27 @@
     _femaleBtn.selected = NO;
 }
 
+// 返回键
 - (IBAction)gobackClick:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
 
+// 性别男
 - (IBAction)selectMale:(id)sender
 {
     _maleBtn.selected = YES;
     _femaleBtn.selected = NO;
 }
 
+// 性别女
 - (IBAction)selectFemale:(id)sender
 {
     _maleBtn.selected = NO;
     _femaleBtn.selected = YES;
 }
 
-// 注册
+// 提交注册
 - (IBAction)SubmitRegistration:(id)sender
 {
     [self requestRegisterData];
@@ -124,13 +127,51 @@
         [MBProgressHUD showError:@"请输入密码！"];
         return;
     }
-
-    
+    if (!(_nameTF.text.length > 0)) {
+        [MBProgressHUD showError:@"请输入姓名！"];
+        return;
+    }
+    if (!(_ageTF.text.length > 0)) {
+        [MBProgressHUD showError:@"请输入年龄！"];
+        return;
+    } else {
+        NSInteger age = [_ageTF.text integerValue];
+        if (age < 4 || age > 12) {
+            [MBProgressHUD showError:@"你的年龄不符合活动要求！"];
+            return;
+        }
+    }
+    if (!(_cityTF.text.length > 0)) {
+        [MBProgressHUD showError:@"请输入市！"];
+        return;
+    }
+    if (!(_countyTF.text.length > 0)) {
+        [MBProgressHUD showError:@"请输入县！"];
+        return;
+    }
+    if (!(_schoolTF.text.length > 0)) {
+        [MBProgressHUD showError:@"请输入学校！"];
+        return;
+    }
+    if (!(_spareInfo.text.length > 0)) {
+        [MBProgressHUD showError:@"请输入备用联系方式！"];
+        return;
+    }
    
     [[[UIApplication sharedApplication] keyWindow] endEditing:YES];
     
+    NSString *gender = @"0";
+    if (_maleBtn.isSelected) {
+        gender = @"1";
+    }
+    NSString *schoolName = [NSString stringWithFormat:@"%@%@%@", _cityTF.text, _countyTF.text, _schoolTF.text];
+    
     NSDictionary *parameters = @{@"number" : _mobilePhoneTF.text? _mobilePhoneTF.text : @"",
-                                 @"password" : _passWordTF.text? _passWordTF.text : @""
+                                 @"password" : _passWordTF.text? _passWordTF.text : @"",
+                                 @"terminalName" : _nameTF.text, // 姓名
+                                 @"terminalType" : gender, // 性别
+                                 @"terminalAddress" : schoolName, // 学校
+                                 @"field1" : _ageTF.text // 年龄
                                  };
     [CommonFunc showLoadingWithTips:@""];
     [requestDataManager getRequestJsonDataWithUrl:LovelyBabyRegister parameters:parameters success:^(id  _Nullable responseObject) {
